321451b8bcdc393182a26ee2e768ea41b8e1b562
Vidshalgit12 <vishalldh_mehta@yahoo.com>
2024-01-07 23:45:27 +1300

    Committing Test2 to BranchX

--- src/Test2.java
+++ src/Test2.java
@@ -1,10 +1,10 @@
 
-public class Test1 {
+public class Test2 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		String hun="Yes";
-		if(hun=="yes" || hun=="Yes" || hun=="YES")
+		if(hun=="yes" || hun=="YES" ||hun=="Y")
 		{
 			System.out.println ("Eat Som");
 		}
