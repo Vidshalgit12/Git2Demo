6353f730ee460b0160bfa989bdbb6614a9c313db
Vidshalgit12 <vishalldh_mehta@yahoo.com>
2024-01-08 00:08:20 +1300

    Added some more Validations

--- src/Test2.java
+++ src/Test2.java
@@ -3,8 +3,10 @@ public class Test2 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		String hun="Yes";
-		if(hun=="yes" || hun=="YES" ||hun=="Y")
+		String hun="YES";
+		String ex=hun.toUpperCase();
+		System.out.println(hun.toUpperCase()+" "+ex);
+		if(ex=="YES")
 		{
 			System.out.println ("Eat Som");
 		}
